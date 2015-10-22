0686312ff419930081da447273e3ac3e9a23166f
bruce-zhu <zhuzhengjing@gmail.com>
2015-10-22 18:10:19 +0800

    设置设备在线状态

--- device_server/tcp_server.js
+++ device_server/tcp_server.js
@@ -6,8 +6,15 @@ var net = require('net');
 var Db = require('./device_db');
 
 var server = net.createServer(function(c) { //'connection' listener
+
     var client = c.remoteAddress + ':' + c.remotePort;
     console.log('client connected[%s]', client);
+
+    // 设置设备在线状态
+
+
+
+
 
     c.on('end', function() {
         console.log('client disconnected[%s]', client);
@@ -18,37 +25,7 @@ var server = net.createServer(function(c) { //'connection' listener
         var recv_json;
 
         try {
-            recv_json = JSON.parse('{\
-            "name": "MICO3288",\
-                "device_id": "HX2501",\
-                "sensor":\
-            [\
-                {\
-                    "type": 1,\
-                    "value": 23.5\
-                },\
-                {\
-                    "type":2,\
-                    "value":25\
-                },\
-                {\
-                    "type":3,\
-                    "value":\
-                    {\
-                        "pm2_5": 23.6,\
-                        "pm10":102\
-                    }\
-                },\
-                {\
-                    "type": 4,\
-                    "value":\
-                    {\
-                        "longitude": 121.2356,\
-                        "latitude": 31.2345\
-                    }\
-                }\
-            ]\
-        }');
+            recv_json = JSON.parse(data);
         } catch (e) {
             console.log('recv data error: ' + e);
             return;
@@ -64,15 +41,12 @@ var server = net.createServer(function(c) { //'connection' listener
             if (err) console.log('save data to db error: ' + err);
         });
     });
-
-    c.pipe(c);
 });
 
 server.on('error', function(e) {
-    console.log('server error:' + e);
+    console.error('tcp server error:' + e);
 });
 
 server.listen(8124, function() { //'listening' listener
-    console.log('server bound');
+    console.log('tcp server bound');
 });
-
