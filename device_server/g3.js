d8a2a48072a3a6c3186cfd4a1ab9c05d70be06ab
bruce-zhu <zhuzhengjing@gmail.com>
2015-11-10 17:31:41 +0800

    update

--- device_server/g3.js
+++ device_server/g3.js
@@ -3,7 +3,6 @@
  */
 
 var net = require('net');
-var db = require('./device_db');
 var tcp_server_port = require('../settings.js');
 
 // 树莓派只有一个串口,默认被用来做console了,需要先禁用
@@ -28,7 +27,6 @@ wpi.pwmWrite(GPIO_PWM, 0);
 var GPIO_PM2_5 = 4;
 wpi.pinMode(GPIO_PM2_5, wpi.OUTPUT);
 
-
 // ---- Serial ----
 var serialPort = new SerialPort(SERIAL_PORT, {
     baudrate: 9600
@@ -41,17 +39,17 @@ var serial_package_index = 0;
 var serial_package_array = [];
 
 // 每次读取15个点,前面10个丢弃,后面5个计算平均值并保存到数据库.然后休眠2分钟
-var handle_real_pm25 = function(package) {
+var handle_real_pm25 = function(data_package) {
     serial_package_index++;
 
     if (serial_package_index == 10) {
-        // 清空之前的数据
+        // 接收数据之前清空之前的数据
         serial_package_array.length = 0;
-        serial_package_array.push(package);
+        serial_package_array.push(data_package);
     } else if (serial_package_index > 10 && serial_package_index < 15) {
-        serial_package_array.push(package);
+        serial_package_array.push(data_package);
     } else if (serial_package_index == 15) {
-        serial_package_array.push(package);
+        serial_package_array.push(data_package);
         serial_package_index = 0;
         // 关闭PM2.5传感器
         wpi.digitalWrite(GPIO_PM2_5, 0);
@@ -67,22 +65,20 @@ var handle_real_pm25 = function(package) {
         pm2_5_average = pm2_5_average / serial_package_array.length;
         pm10_average = pm10_average / serial_package_array.length;
 
-        var data_save = '{\
-                "name": "RPi PM2.5 Sensor",\
-                "device_id": "G3-RPi-1100000",\
-                "sensor":\
-                [\
-                    {\
-                        "type":3,\
-                        "value":\
-                        {\
-                            "pm1_0": pm1_0_average,\
-                            "pm2_5": pm2_5_average,\
-                            "pm10": pm10_average\
-                        }\
-                    }\
-                ]\
-            }';
+        var data_save = {
+            name: "RPi PM2.5 Sensor",
+            device_id: "G3-RPi-1100000",
+            sensor: [
+                {
+                    type: 3,
+                    value: {
+                        pm1_0: pm1_0_average,
+                        pm2_5: pm2_5_average,
+                        pm10: pm10_average
+                    }
+                }
+            ]
+        };
 
         client.write(JSON.stringify(data_save));
 
@@ -95,27 +91,27 @@ var handle_real_pm25 = function(package) {
 };
 
 var g3 = function() {
-
-    // 高电平打开G3传感器
-    wpi.digitalWrite(GPIO_PM2_5, 1);
-
-    client_function();
-
     serialPort.on("open", function () {
         console.log(SERIAL_PORT + ' open success');
 
+        // 高电平打开G3传感器
+        wpi.digitalWrite(GPIO_PM2_5, 1);
+
+        // 连接TCP服务器
+        client_function();
+
         // 处理完整的package
-        var handle_package = function(package) {
+        var handle_package = function(data_package) {
             console.log('#####################');
-            console.log(package);
+            console.log(data_package);
             // data length should be 24bytes
-            if (package.length !== 24) {
+            if (data_package.length !== 24) {
                 console.log('data package length[24, %d]', package.length);
                 return;
             }
 
             // check data package length, should be 20
-            var package_length = package[2] * 256 + package[3];
+            var package_length = data_package[2] * 256 + data_package[3];
             if (package_length !== 20) {
                 console.log('RECV data package length error[20, %d]', package_length);
                 return;
@@ -123,11 +119,11 @@ var g3 = function() {
 
             // check CRC
             var crc = 0;
-            for (var i = 0; i < package.length - 2; i++) {
-                crc += package[i];
+            for (var i = 0; i < data_package.length - 2; i++) {
+                crc += data_package[i];
             }
             crc = crc % (256*256);
-            var package_crc = package[22] * 256 + package[23];
+            var package_crc = data_package[22] * 256 + data_package[23];
             if (package_crc !== crc) {
                 console.log('data package crc error[%d, %d]', package_crc, crc);
                 return;
@@ -135,22 +131,22 @@ var g3 = function() {
 
             // all is OK, let's get real value
             var index = 4;
-            if (package[0] === 0x42 && package[1] === 0x4d) {
+            if (data_package[0] === 0x42 && data_package[1] === 0x4d) {
                 // PM1.0(CF=1)
-                var pm1_0 = package[index++] * 256 + package[index++];
+                var pm1_0 = data_package[index++] * 256 + data_package[index++];
                 // PM2.5(CF=1)
-                var pm2_5 = package[index++] * 256 + package[index++];
+                var pm2_5 = data_package[index++] * 256 + data_package[index++];
                 // PM10(CF=1)
-                var pm10 = package[index++] * 256 + package[index++];
+                var pm10 = data_package[index++] * 256 + data_package[index++];
 
                 console.log('(CF=1) -> [%d, %d, %d]', pm1_0, pm2_5, pm10);
 
                 // PM1.0(大气环境下)
-                var pm_air_1_0 = package[index++] * 256 + package[index++];
+                var pm_air_1_0 = data_package[index++] * 256 + data_package[index++];
                 // PM2.5(大气环境下)
-                var pm_air_2_5 = package[index++] * 256 + package[index++];
+                var pm_air_2_5 = data_package[index++] * 256 + data_package[index++];
                 // PM10(大气环境下)
-                var pm_air_10 = package[index++] * 256 + package[index++];
+                var pm_air_10 = data_package[index++] * 256 + data_package[index++];
 
                 console.log('大气环境 -> [%d, %d, %d]', pm_air_1_0, pm_air_2_5, pm_air_10);
                 handle_real_pm25({
@@ -162,7 +158,7 @@ var g3 = function() {
                 // 数据7,8,9保留
             } else {
                 console.log('RECV data err: ');
-                console.log(package);
+                console.log(data_package);
             }
         };
 
