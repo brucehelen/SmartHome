f55d1d34ae47b1004486c6d8a9d6e618f2c98bfd
bruce-zhu <zhuzhengjing@gmail.com>
2015-11-24 16:30:12 +0800

    baba

--- settings.js
+++ settings.js
@@ -2,11 +2,10 @@
  * Created by missionhealth on 15/10/22.
  */
 
-
 module.exports = {
     db: 'blog',
     host: 'localhost',
-    port: 27017,
-    server_listen_port: 8124
+    port: 27017,                /* mongodb port */
+    server_listen_port: 8124    /* tcp server liston */
 };
 
