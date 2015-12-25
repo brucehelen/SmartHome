010bf36ffceb3b2d2d65469654c3fe88f6a9373e
brucehelen <sudazzj@qq.com>
2015-12-25 20:35:59 +0800

    debug g3 inside

--- device_server/g3.js
+++ device_server/g3.js
@@ -23,10 +23,11 @@ var TEMP_SENSOR_PATH = '/sys/devices/w1_bus_master1/28-0000051095c9/w1_slave';
 // GPIO_PM2_5: 控制PM2.5传感器打开关闭，1-打开，0-关闭
 var GPIO_PM2_5 = 4;
 
-function initG3Sensor() {
-    // ---- GPIO ----
-    wpi.setup('wpi');
+// ---- GPIO ----
+wpi.setup('wpi');
 
+// 初始化G3 PM2.5传感器
+function initG3Sensor() {
     wpi.pinMode(GPIO_PM2_5, wpi.OUTPUT);
     wpi.digitalWrite(GPIO_PM2_5, 0);
 }
@@ -43,13 +44,16 @@ var serial_package_index = 0;
 var serial_package_array = [];
 
 // 每次读取15个点,前面10个丢弃,后面5个计算平均值并保存到数据库.然后休眠2分钟
-var handle_real_pm25 = function(data_package) {
+function handle_real_pm25 (data_package) {
+
     serial_package_index++;
 
     if (serial_package_index > 10 && serial_package_index < 15) {
         serial_package_array.push(data_package);
     } else if (serial_package_index == 15) {
         serial_package_array.push(data_package);
+
+        // 关闭传感器
         wpi.digitalWrite(GPIO_PM2_5, 0);
         // 计算平均值然后保存数据
         var pm1_0_average = 0, pm2_5_average = 0, pm10_average = 0;
@@ -99,6 +103,16 @@ var handle_real_pm25 = function(data_package) {
                 ]
             };
 
+            console.log('-> ' + JSON.stringify(data_save));
+            serial_package_array.length = 0;
+            // 2分钟后再进行下一轮测试
+            setTimeout(function () {
+                // 打开PM2.5传感器
+                wpi.digitalWrite(GPIO_PM2_5, 1);
+            }, 2*60*1000);
+
+            return;
+
             client.write(JSON.stringify(data_save), function(err) {
                 if (err) {
                     console.log('client write error ' + JSON.stringify(data_save));
@@ -114,9 +128,67 @@ var handle_real_pm25 = function(data_package) {
             });
         });
     }
-};
+}
 
-var g3 = function() {
+// 处理完整的package
+function handle_package(data_package) {
+    // data length should be 24bytes
+    if (data_package.length !== 24) {
+        console.log('data package length[24, %d]', package.length);
+        return;
+    }
+
+    // check data package length, should be 20
+    var package_length = data_package[2] * 256 + data_package[3];
+    if (package_length !== 20) {
+        console.log('RECV data package length error[20, %d]', package_length);
+        return;
+    }
+
+    // check CRC
+    var crc = 0;
+    for (var i = 0; i < data_package.length - 2; i++) {
+        crc += data_package[i];
+    }
+    crc = crc % (256*256);
+    var package_crc = data_package[22] * 256 + data_package[23];
+    if (package_crc !== crc) {
+        console.log('data package crc error[%d, %d]', package_crc, crc);
+        return;
+    }
+
+    // all is OK, let's get real value
+    var index = 4;
+    if (data_package[0] === 0x42 && data_package[1] === 0x4d) {
+        // PM1.0(CF=1)
+        var pm1_0 = data_package[index++] * 256 + data_package[index++];
+        // PM2.5(CF=1)
+        var pm2_5 = data_package[index++] * 256 + data_package[index++];
+        // PM10(CF=1)
+        var pm10 = data_package[index++] * 256 + data_package[index++];
+
+        // PM1.0(大气环境下)
+        var pm_air_1_0 = data_package[index++] * 256 + data_package[index++];
+        // PM2.5(大气环境下)
+        var pm_air_2_5 = data_package[index++] * 256 + data_package[index++];
+        // PM10(大气环境下)
+        var pm_air_10 = data_package[index++] * 256 + data_package[index++];
+
+        handle_real_pm25({
+            pm_air_1_0: pm_air_1_0,
+            pm_air_2_5: pm_air_2_5,
+            pm_air_10: pm_air_10
+        });
+
+        // 数据7,8,9保留
+    } else {
+        console.log('RECV data err: ');
+        console.log(data_package);
+    }
+}
+
+// 控制入口函数
+function g3() {
 
     // 初始化G3传感器
     initG3Sensor();
@@ -124,72 +196,18 @@ var g3 = function() {
     serialPort.on("open", function () {
         console.log(SERIAL_PORT + ' open success');
 
-        // 高电平打开G3传感器
+        // 打开G3传感器
         wpi.digitalWrite(GPIO_PM2_5, 1);
 
         // 连接TCP服务器
-        client_function();
-
-        // 处理完整的package
-        var handle_package = function(data_package) {
-            // data length should be 24bytes
-            if (data_package.length !== 24) {
-                console.log('data package length[24, %d]', package.length);
-                return;
-            }
-
-            // check data package length, should be 20
-            var package_length = data_package[2] * 256 + data_package[3];
-            if (package_length !== 20) {
-                console.log('RECV data package length error[20, %d]', package_length);
-                return;
-            }
-
-            // check CRC
-            var crc = 0;
-            for (var i = 0; i < data_package.length - 2; i++) {
-                crc += data_package[i];
-            }
-            crc = crc % (256*256);
-            var package_crc = data_package[22] * 256 + data_package[23];
-            if (package_crc !== crc) {
-                console.log('data package crc error[%d, %d]', package_crc, crc);
-                return;
-            }
-
-            // all is OK, let's get real value
-            var index = 4;
-            if (data_package[0] === 0x42 && data_package[1] === 0x4d) {
-                // PM1.0(CF=1)
-                var pm1_0 = data_package[index++] * 256 + data_package[index++];
-                // PM2.5(CF=1)
-                var pm2_5 = data_package[index++] * 256 + data_package[index++];
-                // PM10(CF=1)
-                var pm10 = data_package[index++] * 256 + data_package[index++];
-
-                // PM1.0(大气环境下)
-                var pm_air_1_0 = data_package[index++] * 256 + data_package[index++];
-                // PM2.5(大气环境下)
-                var pm_air_2_5 = data_package[index++] * 256 + data_package[index++];
-                // PM10(大气环境下)
-                var pm_air_10 = data_package[index++] * 256 + data_package[index++];
-
-                handle_real_pm25({
-                    pm_air_1_0: pm_air_1_0,
-                    pm_air_2_5: pm_air_2_5,
-                    pm_air_10: pm_air_10
-                });
-
-                // 数据7,8,9保留
-            } else {
-                console.log('RECV data err: ');
-                console.log(data_package);
-            }
-        };
+        //client_function();
 
         var whole_package = new Buffer(PACKAGE_LEN);
         var package_index = 0;
         serialPort.on('data', function(data) {
+
+            console.log('-> ' + data);
+
             for (var i = 0; i < data.length; i++) {
                 // check package header
                 if (package_index === 0) {
@@ -211,9 +229,9 @@ var g3 = function() {
     serialPort.on('error', function(err) {
         console.log('Open serial port error: ' + err);
     });
-};
+}
 
-var client_function = function() {
+function client_function() {
     client = net.connect({port: tcp_server_port.server_listen_port}, function() { //'connect' listener
         console.log('G3: connected to server!');
     });
@@ -225,7 +243,6 @@ var client_function = function() {
             console.log(control_data);
         } catch(e) {
             console.log('*** recv data error: ' + e);
-            return;
         }
     });
 
@@ -236,6 +253,6 @@ var client_function = function() {
     client.on('error', function(err) {
         console.log('G3: tcp server, ' + err);
     });
-};
+}
 
 module.exports = g3;
